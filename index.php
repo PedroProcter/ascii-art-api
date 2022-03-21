26c0018694740ea623512d0b9ce69f5e7fc91ca4
PedroProcter <oprocter89@gmail.com>
2022-03-21 13:46:10 -0300

    feat(api): Implement a basic api

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'inc/include.php';
+
+$font = "basic-hash";
+if (isset($_GET["font"])) {
+    $font = strtolower($_GET["font"]);
+}
+
+$text = "HI MOM";
+if (isset($_GET["text"])) {
+    $text = strtoupper($_GET["text"]);
+}
+$text = str_split($text);
+
+$letters = array();
+
+foreach ($text as $letter) {
+    array_push($letters, $fonts[$font][$letter]);
+}
+
+$lines = array();
+
+foreach ($letters as $letter) {
+    $letter_lines = explode("\n", $letter); 
+    for ($line = 0; $line < count($letter_lines); $line += 1) {
+        $lines[$line] .= $letter_lines[$line];
+    }
+}
+
+header("Content-Type: text-plain");
+
+foreach ($lines as $line) {
+    echo $line . "\n";
+}
